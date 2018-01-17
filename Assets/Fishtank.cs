50f6a17fb7a3c1ab467901789f4fe6f3684821f6
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-01-17 17:51:47 +1300

    init

---
+++ Assets/Fishtank.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishtank : MonoBehaviour {
+
+	public GameObject monomerPrefab;
+	public int numMonomers = 50;
+	private Dictionary<GameObject, GameObject> pairs;
+	private Dictionary<GameObject, bool> isTop;
+	public float pairingVelocity = .05f;
+	public int rotationVelocity = 50;
+	private Bounds bounds;
+
+	void FindPairs()
+	{
+		pairs = new Dictionary<GameObject, GameObject>();
+		isTop = new Dictionary<GameObject, bool>();
+		var monomers = GameObject.FindGameObjectsWithTag("monomer");
+		Debug.Log("There are " + monomers.Length + " monomers around");
+		foreach (var a in monomers)
+		{
+			if (pairs.ContainsKey(a))
+			{
+				// Already know the pair for this
+				continue;
+			}
+			var minDistance = float.PositiveInfinity;
+			var match = a;
+			foreach (var b in monomers)
+			{
+				if (a != b && !pairs.ContainsKey(b)) // Prevent love triangles
+				{
+					float dist = Vector3.Distance(a.transform.position, b.transform.position);
+					if (dist < minDistance)
+					{
+						minDistance = dist;
+						match = b;
+					}
+				}
+			}
+			Debug.Log(a.name + "'s closest pair is " + match.name + " with distance " + minDistance);
+			pairs[a] = match;
+			pairs[match] = a;
+			isTop[a] = true;
+			isTop[match] = false;
+		}
+	}
+
+	void PushMonomersTogether()
+	{
+		var monomers = GameObject.FindGameObjectsWithTag("monomer");
+		foreach (var monomer in monomers)
+		{
+			var partner = pairs[monomer];
+			var targetPos = partner.transform.position;
+			var offset = new Vector3(.0141f, -.0025f, .0066f);
+			var rotationalOffset = new Vector3(0, 180, 0);
+			
+			if (isTop[monomer])
+			{
+				targetPos += offset;
+			}
+			else
+			{
+				targetPos -= offset;
+			}
+			monomer.transform.position = Vector3.MoveTowards(monomer.transform.position, targetPos, Time.deltaTime * pairingVelocity);
+			var partnersRotation = partner.transform.rotation.eulerAngles;
+			if (isTop[monomer])
+			{
+				var targetRotation = partnersRotation + rotationalOffset;
+				monomer.transform.rotation = Quaternion.RotateTowards(monomer.transform.rotation, Quaternion.Euler(targetRotation), Time.deltaTime * rotationVelocity);
+			}
+			else
+			{
+				var targetRotation = Vector3.zero;
+				monomer.transform.rotation = Quaternion.RotateTowards(monomer.transform.rotation, Quaternion.Euler(targetRotation), Time.deltaTime * rotationVelocity);
+			}
+			if (!bounds.Contains(monomer.transform.position))
+			{
+				// Wayward monomer
+				monomer.transform.position = Vector3.MoveTowards(monomer.transform.position, bounds.center, Time.deltaTime * pairingVelocity);
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		bounds = gameObject.GetComponent<Collider>().bounds;
+		var b = bounds.extents;
+		for (int i = 0; i < numMonomers; i++)
+		{
+			var monomer = Instantiate(monomerPrefab, transform);
+			var pos = transform.position + new Vector3(Random.Range(-b.x, b.x), Random.Range(-b.y, b.y), Random.Range(-b.z, b.z));
+			monomer.transform.position = pos;
+			monomer.transform.rotation = Random.rotation;
+			monomer.name = "monomer_" + i;	
+		}
+		InvokeRepeating("FindPairs", 0, .1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PushMonomersTogether();
+	}
+}
---
+++ Assets/Pickupable.cs
@@ -0,0 +1,201 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Basic throwable object
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+namespace Valve.VR.InteractionSystem
+{
+	//-------------------------------------------------------------------------
+	[RequireComponent(typeof(Interactable))]
+	public class Pickupable : MonoBehaviour
+	{
+		[EnumFlags]
+		[Tooltip("The flags used to attach this object to the hand.")]
+		public Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.DetachFromOtherHand;
+
+		[Tooltip("Name of the attachment transform under in the hand's hierarchy which the object should should snap to.")]
+		public string attachmentPoint;
+
+		[Tooltip("How fast must this object be moving to attach due to a trigger hold instead of a trigger press?")]
+		public float catchSpeedThreshold = 0.0f;
+
+		[Tooltip("When detaching the object, should it return to its original parent?")]
+		public bool restoreOriginalParent = false;
+
+		public bool attachEaseIn = false;
+		public AnimationCurve snapAttachEaseInCurve = AnimationCurve.EaseInOut(0.0f, 0.0f, 1.0f, 1.0f);
+		public float snapAttachEaseInTime = 0.15f;
+		public string[] attachEaseInAttachmentNames;
+
+		private VelocityEstimator velocityEstimator;
+		public bool attached = false;
+		private float attachTime;
+		private Vector3 attachPosition;
+		private Quaternion attachRotation;
+		private Transform attachEaseInTransform;
+
+		public UnityEvent onPickUp;
+		public UnityEvent onDetachFromHand;
+
+		public bool snapAttachEaseInCompleted = false;
+
+
+		//-------------------------------------------------
+		void Awake()
+		{
+			if (attachEaseIn)
+			{
+				attachmentFlags &= ~Hand.AttachmentFlags.SnapOnAttach;
+			}
+		}
+
+
+		//-------------------------------------------------
+		private void OnHandHoverBegin(Hand hand)
+		{
+			bool showHint = true;
+
+			// "Catch" the throwable by holding down the interaction button instead of pressing it.
+			// Only do this if the throwable is moving faster than the prescribed threshold speed,
+			// and if it isn't attached to another hand
+			if (!attached)
+			{
+				if (hand.GetStandardInteractionButton())
+				{
+					hand.AttachObject(gameObject, attachmentFlags, attachmentPoint);
+				}
+			}
+
+			if (showHint)
+			{
+				ControllerButtonHints.ShowButtonHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+			}
+		}
+
+
+		//-------------------------------------------------
+		private void OnHandHoverEnd(Hand hand)
+		{
+			ControllerButtonHints.HideButtonHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+		}
+
+
+		//-------------------------------------------------
+		private void HandHoverUpdate(Hand hand)
+		{
+			//Trigger got pressed
+			if (hand.GetStandardInteractionButtonDown())
+			{
+				hand.AttachObject(gameObject, attachmentFlags, attachmentPoint);
+				ControllerButtonHints.HideButtonHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+			}
+		}
+
+		//-------------------------------------------------
+		private void OnAttachedToHand(Hand hand)
+		{
+			attached = true;
+
+			onPickUp.Invoke();
+
+			hand.HoverLock(null);
+
+			attachTime = Time.time;
+			attachPosition = transform.position;
+			attachRotation = transform.rotation;
+
+			if (attachEaseIn)
+			{
+				attachEaseInTransform = hand.transform;
+				if (!Util.IsNullOrEmpty(attachEaseInAttachmentNames))
+				{
+					float smallestAngle = float.MaxValue;
+					for (int i = 0; i < attachEaseInAttachmentNames.Length; i++)
+					{
+						Transform t = hand.GetAttachmentTransform(attachEaseInAttachmentNames[i]);
+						float angle = Quaternion.Angle(t.rotation, attachRotation);
+						if (angle < smallestAngle)
+						{
+							attachEaseInTransform = t;
+							smallestAngle = angle;
+						}
+					}
+				}
+			}
+
+			snapAttachEaseInCompleted = false;
+		}
+
+
+		//-------------------------------------------------
+		private void OnDetachedFromHand(Hand hand)
+		{
+			attached = false;
+
+			onDetachFromHand.Invoke();
+
+			hand.HoverUnlock(null);
+		}
+
+
+		//-------------------------------------------------
+		private void HandAttachedUpdate(Hand hand)
+		{
+			//Trigger got released
+			if (!hand.GetStandardInteractionButton())
+			{
+				// Detach ourselves late in the frame.
+				// This is so that any vehicles the player is attached to
+				// have a chance to finish updating themselves.
+				// If we detach now, our position could be behind what it
+				// will be at the end of the frame, and the object may appear
+				// to teleport behind the hand when the player releases it.
+				StartCoroutine(LateDetach(hand));
+			}
+
+			if (attachEaseIn)
+			{
+				float t = Util.RemapNumberClamped(Time.time, attachTime, attachTime + snapAttachEaseInTime, 0.0f, 1.0f);
+				if (t < 1.0f)
+				{
+					t = snapAttachEaseInCurve.Evaluate(t);
+					transform.position = Vector3.Lerp(attachPosition, attachEaseInTransform.position, t);
+					transform.rotation = Quaternion.Lerp(attachRotation, attachEaseInTransform.rotation, t);
+				}
+				else if (!snapAttachEaseInCompleted)
+				{
+					gameObject.SendMessage("OnThrowableAttachEaseInCompleted", hand, SendMessageOptions.DontRequireReceiver);
+					snapAttachEaseInCompleted = true;
+				}
+			}
+		}
+
+
+		//-------------------------------------------------
+		private IEnumerator LateDetach(Hand hand)
+		{
+			yield return new WaitForEndOfFrame();
+
+			hand.DetachObject(gameObject, restoreOriginalParent);
+		}
+
+
+		//-------------------------------------------------
+		private void OnHandFocusAcquired(Hand hand)
+		{
+			gameObject.SetActive(true);
+		}
+
+
+		//-------------------------------------------------
+		private void OnHandFocusLost(Hand hand)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
