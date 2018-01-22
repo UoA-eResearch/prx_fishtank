76efea25df4a0accb67bc407addaa5d9926c0033
Bianca <b.haux@auckland.ac.nz>
2018-01-22 15:16:15 +1300

    Break dimers into monomers on shaking.

--- Assets/BreakDimer.cs
+++ Assets/BreakDimer.cs
@@ -7,6 +7,12 @@ public class BreakDimer : MonoBehaviour {
 
 	public GameObject monomerPrefab;
 	public bool shouldMonomerise = true;
+	private VelocityEstimator velEst;
+	private bool dimerAttached = false;
+
+	void Awake(){
+		velEst = GetComponent<VelocityEstimator> ();
+	}
 
 	void OnHandHoverBegin(Hand hand)
 	{
@@ -39,6 +45,45 @@ public class BreakDimer : MonoBehaviour {
 				hand.otherHand.AttachObject(monomer2, attachmentFlags);
 			}
 		}
+	}
+
+	void HandAttachedUpdate(Hand hand){
+		if(gameObject == hand.currentAttachedObject && shouldMonomerise){
+			Vector3 velocity = velEst.GetVelocityEstimate ();
+			Debug.Log ("Velocity: " + velocity + velocity.magnitude);
+
+			if (velocity.magnitude > 1.0 && dimerAttached) {
+				breakDimerOnShake (hand);
+				dimerAttached = false;
+			}
+		}
+	}
+
+	void breakDimerOnShake(Hand hand){
+		GameObject.Find ("fishtank").GetComponent<Fishtank> ().shouldDimerise = false;
+		// Drop whatever you're holding
+		hand.DetachObject(hand.currentAttachedObject);
+		Debug.Log(gameObject.name + " is attached to " + hand.name);
+		// Make a monomer and attach it to the hand. This replaces the dimer you were just holding.
+		var monomerPos = transform.Find("monomerPos");
+		var monomer1 = Instantiate(monomerPrefab, monomerPos.position, monomerPos.rotation, transform.parent);
+		var partnerPos = monomer1.transform.Find("partnerPos");
+		var monomer2 = Instantiate(monomerPrefab, partnerPos.position, partnerPos.rotation, transform.parent);
+		var names = gameObject.name.Split();
+		monomer1.name = names[2];
+		monomer2.name = names[4];
+		Debug.Log("Destroying " + gameObject.name);
+		Destroy(gameObject);
+	}
+
+	void OnAttachedToHand(){
+		dimerAttached = true;
+		velEst.BeginEstimatingVelocity ();
+	}
+
+	void OnDetachedFromHand(){
+		dimerAttached = false;
+		velEst.FinishEstimatingVelocity ();
 	}
 
 	private void Update()
