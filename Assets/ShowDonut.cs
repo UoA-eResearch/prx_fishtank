661dcc014ae672c5ee2d66f6649b46388552076b
Nick Young <nick.young@auckland.ac.nz>
2018-03-15 10:32:24 +1300

    donut easter egg

---
+++ Assets/ShowDonut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDonut : MonoBehaviour {
+
+	private GameObject donut;
+
+	private void Awake()
+	{
+		donut = transform.parent.Find("Donut").gameObject;
+	}
+
+	private void DonutOff()
+	{
+		donut.SetActive(false);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "monomer" && !donut.activeInHierarchy)
+		{
+			donut.SetActive(true);
+			Invoke("DonutOff", 5);
+		}
+	}
+}
