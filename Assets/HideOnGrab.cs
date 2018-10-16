28ad6221915292b926a70d67563c86cad90b67e4
warrickct <wcor690@aucklanduni.ac.nz>
2018-10-16 16:22:47 +1300

    Added hiding of UI on trigger use, made new controller labels for grabbing and teleporting. Reorganised project structure slightly.

---
+++ Assets/HideOnGrab.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class HideOnGrab : MonoBehaviour {
+
+	public Hand parentHand;
+
+	public GameObject[] hidableUIElements;
+
+	private void OnEnable() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// NOTE: Put in late update at the moment. Something is messing with a single teleport UI Hint. I suspect the fishtank UpdateViveControllers
+	void LateUpdate()
+	{
+		if (parentHand.controller.GetHairTrigger()) {
+			foreach (var uiElement in hidableUIElements) {
+				Debug.Log(uiElement.name);
+				uiElement.SetActive(false);
+			}
+		} else {
+			foreach (var uiElement in hidableUIElements) {
+				uiElement.SetActive(true);
+			}
+		}
+	}
+}
