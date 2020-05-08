8994dca62da274d3ae7c92b4ab88150fb48f5e5a
Nick Young <nick.young@auckland.ac.nz>
2020-05-08 16:49:26 +1200

    update MRTK-Quest, Oculus plugin

--- createSymlink.bat
+++ createSymlink.bat
@@ -4,25 +4,8 @@ setlocal ENABLEDELAYEDEXPANSION
 
 cd /d %~dp0
 
-set FOLDER_1=MixedRealityToolkit
-set FOLDER_2=MixedRealityToolkit.SDK
-set FOLDER_3=MixedRealityToolkit.Services
-set FOLDER_4=MixedRealityToolkit.Providers
-set FOLDER_5=MixedRealityToolkit.Examples
-set FOLDER_6=MixedRealityToolkit.Extensions
-set FOLDER_7=MixedRealityToolkit.Tools
-
-set i=1
-:BEGIN
-call set f=%%FOLDER_!i!%%
-if defined f (
-  rem echo Create symbolic link !f!
-  mklink /D Assets\!f! ..\MRTK-Quest\External\MixedRealityToolkit-Unity\Assets\!f!
-  mklink Assets\!f!.meta ..\MRTK-Quest\External\MixedRealityToolkit-Unity\Assets\!f!.meta
-  set /A i+=1
-  goto :BEGIN
-)
-
+mklink /D Assets\MRTK ..\MRTK-Quest\External\MixedRealityToolkit-Unity\Assets\MRTK
+mklink Assets\MRTK.meta ..\MRTK-Quest\External\MixedRealityToolkit-Unity\Assets\MRTK.meta
 mklink /D Assets\MixedRealityToolkit.ThirdParty ..\MRTK-Quest\Assets\MixedRealityToolkit.ThirdParty
 mklink Assets\MixedRealityToolkit.ThirdParty.meta ..\MRTK-Quest\Assets\MixedRealityToolkit.ThirdParty.meta
 
